de617915fb1c5f3e729bd01a47febf60152e9bdd
mjansen6631 <mjansen6631@gmail.com>
2023-09-28 17:07:54 -0700

    JS Challenges folder added, if else statements.

---
+++ Unit_2/Conditionals/switch.js
@@ -0,0 +1,8 @@
+/* 
+    Switch Conditional Statement
+    - A switch statement executes/runs a block of code depending on different cases.
+    -The switch is used together with a break or a default keyword or both.
+        - These are both optional.
+        - Break: keyword that breaks out of the switch block. If a break is omitted/not added the next code block in the switch will run (run as well)
+        - Default: keyword that specifies code to run if no case matches (think what or else did in if statements).
+*/
