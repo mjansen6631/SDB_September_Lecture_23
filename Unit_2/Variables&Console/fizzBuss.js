5da215475fc760f984e669a4943f27cf523f4af6
mjansen6631 <mjansen6631@gmail.com>
2023-10-16 16:28:27 -0600

    Unit_3 folder created.

--- Unit_2/Variables&Console/fizzBuss.js
+++ Unit_2/Variables&Console/fizzBuss.js
@@ -49,12 +49,27 @@ fizzBuzz();
 
 function fizzBuzz () {
 
-    for(let i = 1; i <= 100; i++) {
+    for(let i = 1; i <= 30; i++) {
         if(i % 5 == 0 && i % 3 == 0) {
             console.log("Fizz Buzz")
         } else if(i % 3 == 0){
             console.log("Fizz");
         } else if(i % 5 == 0) {
+            console.log("Buzz");
+        } else {console.log(i+"")}
+    }
+}
+
+fizzBuzz();
+
+function fizzBuzz () {
+
+    for(let i = 1; i <= 100; i++) {
+        if(i % 5 == 0 && i % 3 == 0) {
+            console.log("Fizz Buzz")
+        } else if(i % 3 === 0){
+            console.log("Fizz");
+        } else if(i % 5 === 0) {
             console.log("Buzz");
         } else {console.log(i+"")}
     }
